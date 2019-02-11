c1d5a8424d2f1604bbcc4fdda526dc6e7544632b
nac <nac@wustl.edu>
2019-02-11 01:26:28 -0600

    test eclipse commit 01

--- src/test/YourHW2Tests.java
+++ src/test/YourHW2Tests.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +14,10 @@ import org.junit.Test;
 import hw1.Catalog;
 import hw1.Database;
 import hw1.HeapFile;
+import hw1.IntField;
 import hw1.TupleDesc;
+import hw2.AggregateOperator;
+import hw2.Relation;
 
 public class YourHW2Tests {
 
@@ -49,9 +53,13 @@ public class YourHW2Tests {
 		ahf = c.getDbFile(tableId);
 	}
 	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGroupBy() {
+		Relation testr = new Relation(testhf.getAllTuples(), testtd);
+		testr = testr.aggregate(AggregateOperator.MAX, true);
+		
+		assertTrue(testr.getTuples().size() == 128);
 	}
 
 }
