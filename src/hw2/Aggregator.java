3b54a3f53c67c47d8248f875ae0e48154f461e03
nachen <nachen@192.168.1.25>
2019-02-20 01:23:35 -0600

    eclipse commit 03

--- src/hw2/Aggregator.java
+++ src/hw2/Aggregator.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import hw1.IntField;
+import hw1.StringField;
 import hw1.Tuple;
 import hw1.TupleDesc;
+import hw1.Type;
 
 /**
  * A class to perform various aggregations, by accepting one tuple at a time
@@ -17,7 +20,12 @@ public class Aggregator {
 	private AggregateOperator o;
 	private boolean groupBy;
 	private TupleDesc td;
-	private ArrayList<Tuple> tuples;
+	private Tuple tuple = new Tuple(td);
+	private boolean firstTime = true;
+	private int sum;
+	private int count;
+	private String minStr;
+	private String maxStr;
 	
 	public Aggregator(AggregateOperator o, boolean groupBy, TupleDesc td) {
 		//your code here
@@ -32,14 +40,124 @@ public class Aggregator {
 	 */
 	public void merge(Tuple t) {
 		//your code here
-		if (groupBy) {
-			
+		if (firstTime) {
+			tuple = t;
+			firstTime = false;
 		} else {
-			if (o == AggregateOperator.SUM) {
-//				tuples.get(0).getField(1) = tuples.get(0).getField(1) + t.getField(1);
+			if (groupBy) {
+				if (t.getDesc().getType(1) == Type.STRING && td.getType(1) == Type.STRING) {
+					Type[] typeAr = {t.getDesc().getType(0), t.getDesc().getType(1)};
+					String[] fieldAr = {t.getDesc().getFieldName(0), t.getDesc().getFieldName(1)};
+					Tuple tuple2 = new Tuple(new TupleDesc(typeAr, fieldAr));
+					if (o == AggregateOperator.MIN) {
+						
+					} else if (o == AggregateOperator.MAX) {
+						
+					} else if (o == AggregateOperator.COUNT) {
+						count++;
+						tuple2.setField(0, new IntField(count));
+						tuple = tuple2;
+					}
+				} else if (t.getDesc().getType(1) == Type.INT && td.getType(1) == Type.INT) {
+					Type[] typeAr = {t.getDesc().getType(0), t.getDesc().getType(1)};
+					String[] fieldAr = {t.getDesc().getFieldName(0), t.getDesc().getFieldName(1)};
+					Tuple tuple2 = new Tuple(new TupleDesc(typeAr, fieldAr));
+					if (o == AggregateOperator.SUM) {
+						
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						tuple2.setField(0, new IntField(new IntField(data1).getValue() + new IntField(data2).getValue()));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.MIN) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						int d1 = new IntField(data1).getValue();
+						int d2 = new IntField(data2).getValue();
+						tuple2.setField(0, new IntField(d1 < d2 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.MAX) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						int d1 = new IntField(data1).getValue();
+						int d2 = new IntField(data2).getValue();
+						tuple2.setField(0, new IntField(d1 > d2 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.AVG) {
+						count++;
+						sum += new IntField(t.getField(0).toByteArray()).getValue();
+						tuple2.setField(0, new IntField(sum / count));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.COUNT) {
+						count++;
+						tuple2.setField(0, new IntField(count));
+						tuple = tuple2;
+					}
+				}
+
+				
+			} else {
+				if (t.getDesc().getType(0) == Type.STRING && td.getType(0) == Type.STRING) {
+					Type[] typeAr = {Type.STRING};
+					String[] fieldAr = {t.getDesc().getFieldName(1)};
+					Tuple tuple2 = new Tuple(new TupleDesc(typeAr, fieldAr));
+					if (o == AggregateOperator.MIN) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						String d1 = new StringField(data1).getValue();
+						String d2 = new StringField(data2).getValue();
+						tuple2.setField(0, new StringField(d1.compareToIgnoreCase(d2) < 0 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.MAX) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						String d1 = new StringField(data1).getValue();
+						String d2 = new StringField(data2).getValue();
+						tuple2.setField(0, new StringField(d1.compareToIgnoreCase(d2) > 0 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.COUNT) {
+						count++;
+						tuple2.setField(0, new IntField(count));
+						tuple = tuple2;
+					}
+				} else if (t.getDesc().getType(0) == Type.INT && td.getType(0) == Type.INT) {
+					Type[] typeAr = {Type.INT};
+					String[] fieldAr = {t.getDesc().getFieldName(0)};
+					Tuple tuple2 = new Tuple(new TupleDesc(typeAr, fieldAr));
+					if (o == AggregateOperator.SUM) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						tuple2.setField(0, new IntField(new IntField(data1).getValue() + new IntField(data2).getValue()));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.MIN) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						int d1 = new IntField(data1).getValue();
+						int d2 = new IntField(data2).getValue();
+						tuple2.setField(0, new IntField(d1 < d2 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.MAX) {
+						byte[] data1 = t.getField(0).toByteArray();
+						byte[] data2 = tuple.getField(0).toByteArray();
+						int d1 = new IntField(data1).getValue();
+						int d2 = new IntField(data2).getValue();
+						tuple2.setField(0, new IntField(d1 > d2 ? d1 : d2));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.AVG) {
+						count++;
+						sum += new IntField(t.getField(0).toByteArray()).getValue();
+						tuple2.setField(0, new IntField(sum / count));
+						tuple = tuple2;
+					} else if (o == AggregateOperator.COUNT) {
+						count++;
+						tuple2.setField(0, new IntField(count));
+						tuple = tuple2;
+					}
+				}
+
 			}
 		}
 	}
+
 	
 	/**
 	 * Returns the result of the aggregation
@@ -47,6 +165,8 @@ public class Aggregator {
 	 */
 	public ArrayList<Tuple> getResults() {
 		//your code here
+		ArrayList<Tuple> tuples = new ArrayList<>();
+		tuples.add(tuple);
 		return tuples;
 	}
 
