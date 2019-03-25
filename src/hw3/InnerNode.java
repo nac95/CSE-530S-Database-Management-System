577b3107a75a05ce0b014b53462e42cecc5377bf
nachen <nachen@192.168.1.25>
2019-03-25 10:47:16 -0500

    eclipse commit 01

--- src/hw3/InnerNode.java
+++ src/hw3/InnerNode.java
@@ -3,30 +3,72 @@ package hw3;
 import java.util.ArrayList;
 
 import hw1.Field;
+import hw1.IntField;
+import hw1.StringField;
+import hw1.Type;
 
 public class InnerNode implements Node {
 	
+	int degree;
+	ArrayList<Field> keys = new ArrayList<>();
+	ArrayList<Node> children = new ArrayList<>();
+	
 	public InnerNode(int degree) {
 		//your code here
+		this.degree = degree;
 	}
 	
 	public ArrayList<Field> getKeys() {
 		//your code here
-		return null;
+		return keys;
 	}
 	
 	public ArrayList<Node> getChildren() {
 		//your code here
-		return null;
+		return children;
 	}
 
 	public int getDegree() {
 		//your code here
-		return 0;
+		return degree;
 	}
 	
 	public boolean isLeafNode() {
 		return false;
+	}
+	
+	public void addKeys(Field key) {
+		if (keys.size() == 0) {
+			keys.add(key);
+		}
+		boolean add = false;
+		if (key.getType() == Type.INT) {
+			byte[] newKeyByte = key.toByteArray();
+			int newKey = new IntField(newKeyByte).getValue();
+			for (int i = 0; i < keys.size(); i++) {
+				byte[] oldKeyByte = keys.get(i).toByteArray();
+				int oldKey = new IntField(oldKeyByte).getValue();
+				if (newKey < oldKey) {
+					keys.add(i, key);
+					add = true;
+				}
+			}
+		}
+		if (key.getType() == Type.STRING) {
+			byte[] newKeyByte = key.toByteArray();
+			String newKey = new StringField(newKeyByte).getValue();
+			for (int i = 0; i < keys.size(); i++) {
+				byte[] oldKeyByte = keys.get(i).toByteArray();
+				String oldKey = new StringField(oldKeyByte).getValue();
+				if (newKey.compareTo(oldKey) < 0) {
+					keys.add(i, key);
+					add = true;
+				}
+			}
+		}
+		if (!add) {
+			keys.add(key);
+		}
 	}
 
 }
--- src/hw3/LeafNode.java
+++ src/hw3/LeafNode.java
@@ -2,24 +2,68 @@ package hw3;
 
 import java.util.ArrayList;
 
+import hw1.Field;
+import hw1.IntField;
+import hw1.StringField;
+import hw1.Type;
+
 public class LeafNode implements Node {
+	
+	int degree;
+	ArrayList<Entry> entries = new ArrayList<>();
+	
 	
 	public LeafNode(int degree) {
 		//your code here
+		this.degree = degree;
 	}
 	
 	public ArrayList<Entry> getEntries() {
 		//your code here
-		return null;
+		return entries;
 	}
 
 	public int getDegree() {
 		//your code here
-		return 0;
+		return degree;
 	}
 	
 	public boolean isLeafNode() {
 		return true;
+	}
+	
+	public void addKeys(Entry entry) {
+		if (entries.size() == 0) {
+			entries.add(entry);
+		}
+		boolean add = false;
+		if (entry.getField().getType() == Type.INT) {
+			byte[] newEntryByte = entry.getField().toByteArray();
+			int newEntry = new IntField(newEntryByte).getValue();
+			for (int i = 0; i < entries.size(); i++) {
+				byte[] oldEntryByte = entries.get(i).getField().toByteArray();
+				int oldEntry = new IntField(oldEntryByte).getValue();
+				if (newEntry < oldEntry) {
+					entries.add(i, entry);
+					add = true;
+				}
+			}
+		}
+		if (entry.getField().getType() == Type.STRING) {
+			byte[] newEntryByte = entry.getField().toByteArray();
+			String newEntry = new StringField(newEntryByte).getValue();
+			for (int i = 0; i < entries.size(); i++) {
+				byte[] oldEntryByte = entries.get(i).getField().toByteArray();
+				String oldEntry = new StringField(oldEntryByte).getValue();
+				if (newEntry.compareTo(oldEntry) < 0) {
+					entries.add(i, entry);
+					add = true;
+				}
+			}
+		}
+		if (!add) {
+			entries.add(entry);
+		}
 	}
 
 }
